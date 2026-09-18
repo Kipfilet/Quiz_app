359c779a5d0aad4e6dad270eaed852fb9204084c
CrazyCTRL <lukagiljan15@gmail.com>
2026-09-18 14:09:21 +0200

    ADDED random questions for developing right and wrong answers

--- frontend/source/fetch.js
+++ frontend/source/fetch.js
@@ -1,4 +1,10 @@
 questionText = document.getElementById('questionText');
+questionOptions = [
+    document.getElementById('option1'),
+    document.getElementById('option2'),
+    document.getElementById('option3'),
+    document.getElementById('option4')
+];
 questionMaxIndex = 0;
 fetch('source/questions.json')
       .then(res => res.json())
@@ -10,5 +16,9 @@ fetch('source/questions.json')
         }
         randomQuestionIndex = Math.floor(Math.random() * questionMaxIndex);
         questionText.textContent = questions[randomQuestionIndex].question;
+        option1.textContent = questions[randomQuestionIndex].options[0];
+        option2.textContent = questions[randomQuestionIndex].options[1];
+        option3.textContent = questions[randomQuestionIndex].options[2];
+        option4.textContent = questions[randomQuestionIndex].options[3];
       })
-      .catch(err => console.error('Error loading questions.json:', err));
+      .catch(err => console.error('Error loading questions: ', err));
